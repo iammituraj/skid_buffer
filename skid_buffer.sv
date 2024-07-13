2a33aa3962b6dc41e7faf6cc631565b7f5a02fdb
Mitu <iammituraj@gmail.com>
2024-07-13 12:48:44 +0530

    Removed ready register

--- skid_buffer.sv
+++ skid_buffer.sv
@@ -7,7 +7,7 @@
                   - Configurable data width.              
 
    Developer    : Mitu Raj, chip@chipmunklogic.com at Chipmunk Logic ™, https://chipmunklogic.com
-   Notes        : -
+   Notes        : The upstream/downstream pipeline is assumed to be in the same reset domain, hence o_ready is 1'b1 on reset.
    License      : Open-source.
    Date         : Mar-26-2022
 ===============================================================================================================================*/
@@ -16,36 +16,30 @@
                                                       S K I D   B U F F E R
 -------------------------------------------------------------------------------------------------------------------------------*/
 
-module skid_buffer #(
-   
+module skid_buffer #(   
    // Global Parameters   
-   parameter DWIDTH    =  8                                // Data width
-                                                          
+   parameter DWIDTH    =  8                                // Data width                                                          
 ) 
-
 (
    input  logic                clk             ,           // Clock
    input  logic                rstn            ,           // Active-low synchronous reset
    
-   // Input Interface   
+   // Input Interface (Upstream pipeline IF)  
    input  logic [DWIDTH-1 : 0] i_data          ,           // Data in
    input  logic                i_valid         ,           // Data in valid
    output logic                o_ready         ,           // Ready out
    
-   // Output Interface
+   // Output Interface (Downstream pipeline IF)
    output logic [DWIDTH-1 : 0] o_data          ,            // Data out
    output logic                o_valid         ,            // Data out valid
    input  logic                i_ready                      // Ready in
 ) ;
 
-
 /*-------------------------------------------------------------------------------------------------------------------------------
    Internal Registers/Signals
 -------------------------------------------------------------------------------------------------------------------------------*/
-logic                ready_rg   ;        // Ready 
 logic [DWIDTH-1 : 0] data_rg    ;        // Data buffer
 logic                bypass_rg  ;        // Bypass signal to data and data valid muxes
-
 
 /*-------------------------------------------------------------------------------------------------------------------------------
    Synchronous logic
@@ -53,53 +47,36 @@ logic                bypass_rg  ;        // Bypass signal to data and data valid
 always @(posedge clk) begin
    
    // Reset  
-   if (!rstn) begin
-      
+   if (!rstn) begin      
       // Internal Registers
-      ready_rg  <= 1'b0 ;
       data_rg   <= '0   ;     
       bypass_rg <= 1'b1 ;
-
-   end
-   
+   end   
    // Out of reset
-   else begin
-      
+   else begin      
       // Bypass state      
-      if (bypass_rg) begin
-         
-         ready_rg <= 1'b1 ;
-
-         if (!i_ready && i_valid && ready_rg) begin
-            ready_rg  <= 1'b0   ;            
+      if (bypass_rg) begin         
+         if (!i_ready && i_valid) begin
             data_rg   <= i_data ;        // Data skid happened, store to buffer
             bypass_rg <= 1'b0   ;        // To skid mode  
          end 
+      end 
 
-      end
-      
       // Skid state
-      else begin
-         
+      else begin         
          if (i_ready) begin
-            ready_rg  <= 1'b1   ;            
             bypass_rg <= 1'b1   ;        // Back to bypass mode           
          end
-
-      end      
-
+      end
    end
-
 end
-
 
 /*-------------------------------------------------------------------------------------------------------------------------------
    Continuous Assignments
 -------------------------------------------------------------------------------------------------------------------------------*/
-assign o_ready = ready_rg                                   ;        
-assign o_data  = bypass_rg ? i_data  : data_rg              ;        // Data mux
-assign o_valid = bypass_rg ? (i_valid & ready_rg) : 1'b1    ;        // Data valid mux
-
+assign o_ready = bypass_rg                     ;        // Ready to upstream pipeline
+assign o_data  = bypass_rg ? i_data  : data_rg ;        // Data mux
+assign o_valid = bypass_rg ? i_valid : 1'b1    ;        // Data valid mux
 
 endmodule
 
